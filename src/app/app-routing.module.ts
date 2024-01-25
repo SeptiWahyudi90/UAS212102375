3ec2d0b7fd87da64f83fcc2e68ea11fa8838cf37
Muhammad Raafii <106815006+Karamell5@users.noreply.github.com>
2024-01-25 11:24:07 +0700

    UAS212102375

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
+import { ForexComponent } from './forex/forex.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { otentikasiGuard } from './otentikasi.guard';
+import { CuacaComponent } from './cuaca/cuaca.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{ path: "admin", component: AdminComponent },
+{ path: "login", component: LoginComponent },
+{ path: "register", component: RegisterComponent },
+{ path: "dashboard", component: DashboardComponent, canActivate : [otentikasiGuard] },
+{ path: "dashboard2", component: Dashboard2Component, canActivate : [otentikasiGuard] },
+{ path: "dashboard3", component: Dashboard3Component, canActivate : [otentikasiGuard] },
+{ path: "mahasiswa", component: MahasiswaComponent, canActivate : [otentikasiGuard] },
+{ path: "forex", component: ForexComponent, canActivate : [otentikasiGuard] },
+{ path: "cuaca", component: CuacaComponent, canActivate : [otentikasiGuard] },
+{ path: "", redirectTo: "login", pathMatch: "full" }
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot (routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
